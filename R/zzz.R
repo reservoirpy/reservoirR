86f65583b8a2c878f670e766c1cfc9300609719c
kalidouBA <kalidou.ia.mlds@gmail.com>
2022-04-28 12:18:11 +0200

    check reservoirpy is installed

--- R/zzz.R
+++ R/zzz.R
@@ -1,4 +1,12 @@
-#' @importFrom reticulate
+err_reservoirpy <- function(e) {
+  message("Error importing reservoirpy")
+  message("Check if reservoirpy is installed")
+  message("Install by reservoir::install_reservoirpy() and reload library")
+}
+
+#' @import reticulate
+reservoirpy <- NULL
 .onLoad <- function(libname, pkgname) {
-  reservoirpy <<- reticulate::import("reservoirpy")
+  reservoirpy <<- reticulate::import("reservoirpy",convert=FALSE,
+                                     delay_load=list(on_error=err_reservoirpy))
 }
