6613a9a9662aa392b54cca978a87a70d6fcab3e9
borishejblum <boris.hejblum@u-bordeaux.fr>
2022-04-29 23:46:38 +0200

    issue with ouutput type of mackey_glass

--- tests/testthat/tests.R
+++ tests/testthat/tests.R
@@ -1,18 +1,18 @@
 test_that("reservoir", {
   timesteps <- 2500
   tau <- 17
-  X = reservoirpy$datasets$mackey_glass(as.integer(timesteps), tau=as.integer(tau))
+  X <- as.vector(reservoirpy$datasets$mackey_glass(as.integer(timesteps), tau=as.integer(tau)))
 
-  X = 2 * (X - min(X)) / (max(X) - min(X)) - 1
+  X <- 2 * (X - min(X)) / (max(X) - min(X)) - 1
 
-  reservoir = reservoirpy$nodes$Reservoir(as.integer(100))
-  readout = reservoirpy$nodes$Ridge()
+  reservoir <- reservoirpy$nodes$Reservoir(100L)
+  readout <- reservoirpy$nodes$Ridge()
 
-  model = reservoirpy$link(reservoir, readout)
+  model <- reservoirpy$link(reservoir, readout)
   model
 
-  Xtrain = as.matrix(X[1:2001,])
-  Ytrain = as.matrix(X[10:2010,])
+  Xtrain <- as.matrix(X[1:2001,])
+  Ytrain <- as.matrix(X[10:2010,])
 
   Xtrain = array_reshape(Xtrain, c(2001, 1))
   Ytrain = array_reshape(Xtrain, c(2001, 1))
