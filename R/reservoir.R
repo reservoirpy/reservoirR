9090fce31bb98a961de8c923812e637a48e19fb7
kalidouBA <kalidou.ia.mlds@gmail.com>
2022-05-12 16:18:21 +0200

    code reorganization and addition of some functions

--- R/reservoir.R
+++ R/reservoir.R
@@ -2,6 +2,16 @@
 #'
 #'@param nodeType Type of node. Default is \code{"Ridge"}.
 #'
+#'@param units (int) optional 
+#'Number of reservoir units. If None, the number of units will be infered from
+#'the \code{W} matrix shape.
+#'
+#'@paramlr (float) default to 1.0
+#'Neurons leak rate. Must be in :math:\code{[0, 1]}.
+#'
+#'@paramsr (float) optional
+#'Spectral radius of recurrent weight matrix.
+#'
 #'@param otputDim Output dimension of the Node. Dimension of its state.
 #'
 #'@param inputDim Input dimension of the Node.
@@ -12,34 +22,78 @@
 #'
 #'@param ridge float, default to \code{0.0}. L2 regularization parameter. 
 #'
-#'@param inputBias bool, default to \code{TRUE}. If TRUE, then a bias parameter 
+#'@param inputBias bool, default to \code{TRUE}. If \code{TRUE}, then a bias parameter 
 #'will be learned along with output weights.
 #'
+#' @examples
+#' \dontrun{
+#' readout <- createNode("Ridge")
+#' }
 #'@export
 createNode <- function(nodeType = c("Ridge"), 
+                       units = NULL,
+                       lr = 1.0,
+                       sr = NULL,
                        otputDim = NULL, 
                        inputDim = NULL, 
                        name = NULL,
                        ridge = 0.0,
                        inputBias = TRUE,
                        dtype = "float64") {
-  stopifnot(!is.null(nodeType))
   
+  stopifnot(!is.null(nodeType))
+
   if(nodeType == "Ridge"){
-    dtype = np$dtype(dtype)
     node <- reservoirpy$nodes$Ridge(output_dim = otputDim, 
-                                    input_dim = inputDim, 
                                     name = name,
                                     ridge = ridge,
-                                    input_bias = inputBias,
-                                    dtype = dtype)
+                                    input_bias = inputBias)
   }
   else if(nodeType=="Reservoir"){
-    NULL
+    if(!is.null(units))
+      #units <- noquote(paste0(as.integer(units),'L'))
+      node <- reservoirpy$nodes$Reservoir(units = as.integer(units),
+                                        lr = lr,
+                                        sr = sr,
+                                        name = name,
+                                        input_bias = inputBias)
+    else
+      node <- reservoirpy$nodes$Reservoir(units = units,
+                                          lr = lr,
+                                          sr = sr,
+                                          name = name,
+                                          input_bias = inputBias)
   }else{
     NULL
   }
-  return(node)
+  return(py_to_r(node))
+}
+
+
+
+#' Link two :py:class:\code{~.Node} instances to form a :py:class:\code{~.Model}
+#' instance. \code{node1} output will be used as input for \code{node2} in the
+#' created model. This is similar to a function composition operation:
+#' 
+#'@details Can update the state of the node several times
+#'
+#'@param node1 (Node) or (list_of_Node)
+#'Nodes or lists of nodes to link.
+#'
+#'@param node2 (Node) or (list_of_Node)
+#'Nodes or lists of nodes to link.
+#'
+#'@param name (str) optional
+#'Name for the chaining Model.
+#'
+#'@export
+#'
+link <- function(node1, node2, name = NULL){
+  
+  stopifnot(!is.null(node1) & !is.null(node2))
+  
+  link <- reservoirpy$link(node1, node2, name)
+  return(py_to_r(link))
 }
 
 
@@ -104,7 +158,51 @@ fit <- function(node, X, Y, warmup = 0){
   stopifnot(!is.null(node))
   stopifnot(is.array(X) & is.array(Y))
   
-  warmup <- as.integer(warmup)
-  fit <- node$fit(X, Y = Y, warmup = warmup)
-  return(fit)
+  fit <- node$fit(X, Y, warmup = as.integer(warmup))
+  return(py_to_r(fit))
+}
+
+
+
+
+#' @title
+#' Load data from the Mackey-Glass delayed differential equation
+#' 
+#' @description
+#' Mackey-Glass time series \code{[8]_ [9]_}~, computed from the Mackey-Glass
+#' delayed differential equation:
+#' 
+#' @param n_timesteps (int) Number of time steps to compute.
+#' @param tau (int), default to 17 
+#' Time delay :math:`\\tau` of Mackey-Glass equation.
+#' By defaults, equals to 17. Other values can
+#' change the choatic behaviour of the timeseries.
+#' @param a (float) default to 0.2
+#' :math:`a` parameter of the equation.
+#' @param b (float) default to 0.1
+#' :math:`b` parameter of the equation.
+#' @param n (int) default to 10
+#' :math:`n` parameter of the equation.
+#' @param x0 (float), optional, default to 1.2
+#' Initial condition of the timeseries.
+#' @param h (float), default to 1.0
+#' Time delta between two discrete timesteps.
+#' 
+#' @return array of shape (n_timesteps, 1) Mackey-Glass timeseries.
+#' 
+#' @examples
+#' \dontrun{
+#' timeSerie <- generate_data(2500)
+#' timeSerie
+#' }
+#' 
+#' @export
+generate_data <- function(n_timesteps, 
+                          tau=17, a = 0.2, b = 0.1, 
+                          n = 10, x0 = 1.2, h = 1.0){
+  stopifnot(!is.null(n_timesteps))
+  timeSerie <- reservoirpy$datasets$mackey_glass(as.integer(n_timesteps),
+                                                 as.integer(tau),a,b,
+                                                 as.integer(n),x0,h)
+  return(as.vector(timeSerie))
 }
