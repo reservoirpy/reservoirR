d7292d55dc4476589902afc851f31e64aa281eb6
Thomas <thomas.ferte@u-bordeaux.fr>
2022-05-12 11:16:13 +0200

    fix testthat to pass RCMD check

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,3 +1,3 @@
 library(testthat)
 
-test_check("reservoir")
+test_check("Reservoir")
--- tests/testthat/tests.R
+++ tests/testthat/tests.R
@@ -1,4 +1,13 @@
+# helper function to skip tests if we don't have the 'foo' module
+skip_if_no_reservoir <- function() {
+  have_reservoir <- py_module_available("reservoir")
+  if (!have_reservoir)
+    skip("reservoir not available for testing")
+}
+
 test_that("reservoir", {
+  skip_if_no_reservoir()
+  
   timesteps <- 2500
   tau <- 17
   X <- as.vector(reservoirpy$datasets$mackey_glass(as.integer(timesteps), tau=as.integer(tau)))
@@ -11,13 +20,12 @@ test_that("reservoir", {
   model <- reservoirpy$link(reservoir, readout)
   model
 
-  Xtrain <- as.matrix(X[1:2001,])
-  Ytrain <- as.matrix(X[10:2010,])
+  Xtrain <- as.matrix(X[1:2001])
+  Ytrain <- as.matrix(X[10:2010])
 
-  Xtrain = array_reshape(Xtrain, c(2001, 1))
-  Ytrain = array_reshape(Xtrain, c(2001, 1))
-
+  Xtrain = reticulate::array_reshape(Xtrain, c(2001, 1))
+  Ytrain = reticulate::array_reshape(Xtrain, c(2001, 1))
+  
   model = model$fit(X=Xtrain, Y=Ytrain)
   class(model)
-  model$fit(X = Xtrain, Y = Ytrain, )
 })
