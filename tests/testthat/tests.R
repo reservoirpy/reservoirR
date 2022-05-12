5c4484f38862ff147ea18e545c891f9236e670d6
kalidouBA <kalidou.ia.mlds@gmail.com>
2022-05-12 16:22:30 +0200

    use r functions in tests

--- tests/testthat/tests.R
+++ tests/testthat/tests.R
@@ -1,23 +1,18 @@
 test_that("reservoir", {
   timesteps <- 2500
-  tau <- 17
-  X <- as.vector(reservoirpy$datasets$mackey_glass(as.integer(timesteps), tau=as.integer(tau)))
+  X <- generate_data(timesteps)
 
   X <- 2 * (X - min(X)) / (max(X) - min(X)) - 1
+  
+  readout <- createNode("Ridge")
+  reservoir <- createNode("Reservoir", units = 100,  lr=0.2, sr=0.8)
 
-  reservoir <- reservoirpy$nodes$Reservoir(100L)
-  readout <- reservoirpy$nodes$Ridge()
-
-  model <- reservoirpy$link(reservoir, readout)
+  model <- link(reservoir, readout)
   model
 
-  Xtrain <- as.matrix(X[1:2001,])
-  Ytrain <- as.matrix(X[10:2010,])
+  Xtrain <- as.matrix(X[1:2001])
+  Ytrain <- as.matrix(X[10:2010])
 
-  Xtrain = array_reshape(Xtrain, c(2001, 1))
-  Ytrain = array_reshape(Xtrain, c(2001, 1))
-
-  model = model$fit(X=Xtrain, Y=Ytrain)
+  model = fit(model, X=Xtrain, Y=Ytrain)
   class(model)
-  model$fit(X = Xtrain, Y = Ytrain, )
 })
