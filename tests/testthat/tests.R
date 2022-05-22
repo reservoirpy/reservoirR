f0ddadc69e772b5175c506ebd71df1bb8c906c3c
Thomas <thomas.ferte@u-bordeaux.fr>
2022-05-22 14:00:46 +0200

    update test file with forma test and print python config

--- tests/testthat/tests.R
+++ tests/testthat/tests.R
@@ -1,20 +1,25 @@
 test_that("reservoir", {
-  skip_on_cran()
+  testthat::skip_on_cran()
+  
+  print(reticulate::py_config())
   
   timesteps <- 2500
-  X <- generate_data(timesteps)
+  X <- reservoir::generate_data(timesteps)
   
   X <- 2 * (X - min(X)) / (max(X) - min(X)) - 1
   
-  readout <- createNode("Ridge")
-  reservoir <- createNode("Reservoir", units = 100,  lr=0.2, sr=0.8)
+  readout <- reservoir::createNode("Ridge")
+  reservoir <- reservoir::createNode("Reservoir", units = 100,  lr=0.2, sr=0.8)
   
-  model <- link(reservoir, readout)
+  model <- reservoir::link(reservoir, readout)
   model
   
   Xtrain <- as.matrix(X[1:2001])
   Ytrain <- as.matrix(X[10:2010])
   
-  model = fit(model, X=Xtrain, Y=Ytrain)
-  class(model)
+  model = reservoir::fit(model, X=Xtrain, Y=Ytrain)
+  
+  # formal test
+  testthat::expect(class(model)[1] == "reservoirpy.model.Model",
+                   failure_message = "Output of fit function is not a reservoirpy.model.Model object")
 })
