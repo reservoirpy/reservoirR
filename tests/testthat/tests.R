e5e3c86a9b65ab5155aa08b78cb0eb103f8aab8c
Thomas <thomas.ferte@u-bordeaux.fr>
2022-05-22 10:55:44 +0200

    rebase conflicts

--- tests/testthat/tests.R
+++ tests/testthat/tests.R
@@ -2,18 +2,18 @@ test_that("reservoir", {
   skip_on_cran()
   timesteps <- 2500
   X <- generate_data(timesteps)
-
+  
   X <- 2 * (X - min(X)) / (max(X) - min(X)) - 1
   
   readout <- createNode("Ridge")
   reservoir <- createNode("Reservoir", units = 100,  lr=0.2, sr=0.8)
-
+  
   model <- link(reservoir, readout)
   model
-
+  
   Xtrain <- as.matrix(X[1:2001])
   Ytrain <- as.matrix(X[10:2010])
-
+  
   model = fit(model, X=Xtrain, Y=Ytrain)
   class(model)
 })
