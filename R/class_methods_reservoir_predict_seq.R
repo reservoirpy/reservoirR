81e48a4237231d470d64192f00cd0d0adfac2084
Thomas <thomas.ferte@u-bordeaux.fr>
2022-08-23 22:34:54 +0200

    fix notes and warning in class functions

--- R/class_methods_reservoir_predict_seq.R
+++ R/class_methods_reservoir_predict_seq.R
@@ -1,45 +1,49 @@
 #' summary.reservoir_predict_seq
 #'
 #' @param object A reservoir_predict_seq object
+#' @param ... Additional argument (unused)
 #'
 #' @return A dataframe with node activation
 #' @export
-summary.reservoir_predict_seq <- function(object){
+summary.reservoir_predict_seq <- function(object, ...){
   object %>%
     unclass() %>%
     as.data.frame() %>%
     tibble::rowid_to_column(var = "time") %>%
     tidyr::pivot_longer(cols = dplyr::starts_with("V"),
                         names_to = "node") %>%
-    mutate(node = gsub(node, pattern = "V", replacement = ""),
-           node = as.numeric(node)) %>%
+    dplyr::mutate(node = gsub(.data$node, pattern = "V", replacement = ""),
+                  node = as.numeric(.data$node)) %>%
     return()
 }
 
 #' plot.reservoir_predict_seq
 #'
-#' @param object A reservoir_predict_seq object
+#' @param x A reservoir_predict_seq object
+#' @param ... deprecated
 #' @param vec_nodes Number of nodes to plot
 #' @param vec_time Time to plot
 #'
 #' @return A ggplot
+#' @importFrom rlang .data
 #' @export
-plot.reservoir_predict_seq <- function(object,
-                               vec_nodes = c(1:20),
-                               vec_time = NULL){
-  dfsummary <- summary.reservoir_predict_seq(object)
+plot.reservoir_predict_seq <- function(x,
+                                       ...,
+                                       vec_nodes = c(1:20),
+                                       vec_time = NULL){
+  dfsummary <- summary.reservoir_predict_seq(x)
   
   if(is.null(vec_time)) vec_time <- unique(dfsummary$time)
   
   dfplot <- dfsummary %>%
-    dplyr::filter(node %in% vec_nodes,
-                  time %in% vec_time) %>%
-    dplyr::mutate(node = as.factor(node))
+    dplyr::filter(.data$node %in% vec_nodes,
+                  .data$time %in% vec_time) %>%
+    dplyr::mutate(node = as.factor(.data$node))
   
   plot <- ggplot2::ggplot(dfplot,
-                          mapping = ggplot2::aes(x = time,
-                                                 y = value,
-                                                 color = node)) +
+                          mapping = ggplot2::aes(x = .data$time,
+                                                 y = .data$value,
+                                                 color = .data$node)) +
     ggplot2::geom_line() +
     ggplot2::theme_bw() +
     ggplot2::theme(legend.position = "none") +
