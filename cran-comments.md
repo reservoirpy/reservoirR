19e5a39bf8c5c17ba0811171df44b11995867fdf
Thomas <thomas.ferte@u-bordeaux.fr>
2023-03-13 13:26:43 +0100

    update figure folder and tests

--- cran-comments.md
+++ cran-comments.md
@@ -6,7 +6,24 @@ Environment :
 - github action : ubuntu v22.04
 - github action : windows server 2022 v10.0
 - github action : macos v12.6.3
+- check_rhub : Windows Server 2022, R-devel, 64 bit
+- check_win_devel : Windows Server 2022 x64 (build 20348)
 
-0 errors | 0 warnings | 0 note
+* checking CRAN incoming feasibility ... NOTE
+Maintainer: 'Thomas Ferte <thomas.ferte@u-bordeaux.fr>'
 
-* This is a new release.
+New submission
+
+Possibly misspelled words in DESCRIPTION:
+  ESN (16:15)
+  IdEx (23:75)
+  PHDS (24:44)
+  RRI (24:40)
+  reservoirPy (13:72)
+
+* checking for detritus in the temp directory ... NOTE
+Found the following files/directories:
+  'lastMiKTeXException'
+
+We believe the note is ok. The words are well spelled. The detritus temp directory does not seem to be a problem (https://stackoverflow.com/questions/62456137/r-cran-check-detritus-in-temp-directory).
+
--- tests/testthat/tests.R
+++ tests/testthat/tests.R
@@ -1,31 +1,31 @@
-test_that("reservoir", {
+test_that("reservoirnet", {
   testthat::skip_on_cran()
   
   print(reticulate::py_config())
   
   timesteps <- 2500
-  X <- reservoir::generate_data(dataset = "mackey_glass",n_timesteps = timesteps)$mackey_glass
+  X <- reservoirnet::generate_data(dataset = "mackey_glass",n_timesteps = timesteps)$mackey_glass
   
   X <- 2 * (X - min(X)) / (max(X) - min(X)) - 1
-  source <- reservoir::createNode("Input")
-  readout <- reservoir::createNode("Ridge")
-  reservoir <- reservoir::createNode("Reservoir", units = 100,  lr=0.2, sr=0.8)
+  source <- reservoirnet::createNode("Input")
+  readout <- reservoirnet::createNode("Ridge")
+  reservoir <- reservoirnet::createNode("Reservoir", units = 100,  lr=0.2, sr=0.8)
   
-  model <- reservoir::link(reservoir, readout)
+  model <- reservoirnet::link(reservoir, readout)
   
   Xtrain <- as.matrix(X[1:2001])
   Ytrain <- as.matrix(X[10:2010])
   
-  model <- reservoir::reservoirR_fit(model, X=Xtrain, Y=Ytrain)
+  model <- reservoirnet::reservoirR_fit(model, X=Xtrain, Y=Ytrain)
   
   # Classification 
-  japanese_vowels <- reservoir::generate_data(
+  japanese_vowels <- reservoirnet::generate_data(
     dataset = "japanese_vowels",
     repeat_targets=TRUE)$japanese_vowels
   
-  source <- reservoir::createNode("Input")
-  readout <- reservoir::createNode("Ridge",ridge=1e-6)
-  reservoir <- reservoir::createNode("Reservoir", 
+  source <- reservoirnet::createNode("Input")
+  readout <- reservoirnet::createNode("Ridge",ridge=1e-6)
+  reservoir <- reservoirnet::createNode("Reservoir", 
                                      units = 500,  
                                      lr=0.1, sr=0.9)
   
@@ -33,13 +33,13 @@ test_that("reservoir", {
   # Example: [source >> reservoir, source] >> readout
   model <- list(source %>>% reservoir, source) %>>% readout
   
-  model_fit <- reservoir::reservoirR_fit(node = model,
+  model_fit <- reservoirnet::reservoirR_fit(node = model,
                               X = japanese_vowels$X_train, 
                               Y = japanese_vowels$Y_train, 
                               stateful=FALSE,
                               warmup = 2)
   
-  Y_pred <- reservoir::predict_seq(node = model_fit$fit, X = japanese_vowels$X_test,stateful = FALSE)
+  Y_pred <- reservoirnet::predict_seq(node = model_fit$fit, X = japanese_vowels$X_test,stateful = FALSE)
   # formal test
   testthat::expect(class(model)[1] == "reservoirpy.model.Model",
                    failure_message = "Output of fit function is not a reservoirpy.model.Model object")
